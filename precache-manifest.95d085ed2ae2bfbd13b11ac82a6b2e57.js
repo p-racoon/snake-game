2f76282dd37ecb2d90e9c784e6b86c71ef2ae0b6
prasham ashesh <prasham.ashesh@infosys.com>
2019-08-17 12:25:50 +0530

    Updates

---
+++ precache-manifest.95d085ed2ae2bfbd13b11ac82a6b2e57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48625e5baf6dad7c886967714bdd2732",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "3f3882c9864ad18dc311",
+    "url": "/snake-game/static/css/main.ecb6e0fb.chunk.css"
+  },
+  {
+    "revision": "c2909a210f93ab086b55",
+    "url": "/snake-game/static/js/2.53704dbc.chunk.js"
+  },
+  {
+    "revision": "3f3882c9864ad18dc311",
+    "url": "/snake-game/static/js/main.9c42952e.chunk.js"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  }
+]);
